b107449c6ca540cc2f2177f827180d0cdf28432e
nhanlun <ntnhan18@apcs.vn>
2020-12-24 18:50:29 +0700

    Now can start Record from run fragment

--- app/src/main/java/com/example/techdash/fragments/RunFragment.java
+++ app/src/main/java/com/example/techdash/fragments/RunFragment.java
@@ -1,16 +1,21 @@
 package com.example.techdash.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.techdash.R;
+import com.example.techdash.activities.RecordRunActivity;
+import com.example.techdash.models.Route;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,6 +29,8 @@ public class RunFragment extends Fragment {
     private static final int REQUEST_CODE = 123;
     private MapView mapView;
     private GoogleMap map;
+    private ImageButton startButton;
+    private Route route;
 
     public RunFragment() {
         // Required empty public constructor
@@ -39,6 +46,15 @@ public class RunFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_run, container, false);
+        startButton = view.findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireActivity(), RecordRunActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
@@ -57,11 +73,14 @@ public class RunFragment extends Fragment {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
-
-        } else {
-
+            if (resultCode == 11111) {
+                route = (Route) data.getSerializableExtra("route");
+                Log.d(TAG, "Successfully receive the route");
+                // TODO: encode and save
+            }
         }
     }
 
