f8f43bc96167e2ca8c1e64eec9ed79ed0c1cdba1
nhanlun <ntnhan18@apcs.vn>
2020-12-24 23:43:04 +0700

    Working on saving record

--- app/src/main/java/com/example/techdash/activities/RecordRunActivity.java
+++ app/src/main/java/com/example/techdash/activities/RecordRunActivity.java
@@ -3,13 +3,21 @@ package com.example.techdash.activities;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.techdash.R;
 import com.example.techdash.broadcasts.RecordBroadcast;
+import com.example.techdash.models.Route;
 import com.example.techdash.repositories.RecordRunRepository;
 import com.example.techdash.services.RecordService;
+import com.example.techdash.viewmodels.RecordViewModel;
+import com.google.maps.android.PolyUtil;
 
 public class RecordRunActivity extends AppCompatActivity {
 
@@ -18,16 +26,25 @@ public class RecordRunActivity extends AppCompatActivity {
     private Intent intent;
     private IntentFilter intentFilter;
     private RecordBroadcast broadcast;
+    private RecordViewModel recordViewModel;
+    private NavController navController;
+    private String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_run);
 
+        intent = getIntent();
+        uid = intent.getStringExtra("uid");
+
         intent = new Intent(this, RecordService.class);
         intentFilter = new IntentFilter();
         intentFilter.addAction(getString(R.string.intent_action));
         broadcast = new RecordBroadcast();
+        recordViewModel = new ViewModelProvider(this).get(RecordViewModel.class);
+
+        navController = Navigation.findNavController(this, R.id.fragment);
     }
 
     @Override
@@ -43,5 +60,13 @@ public class RecordRunActivity extends AppCompatActivity {
         unregisterReceiver(broadcast);
         RecordRunRepository.getInstance().removeDataSource(broadcast.getRoute());
         super.onStop();
+    }
+
+    public void stopRecordActivity(View view) {
+        stopService(intent);
+        Route route = recordViewModel.getRoute().getValue();
+        String encoded = PolyUtil.encode(route.getListLatLng());
+        Log.d(TAG, encoded);
+        RecordRunRepository.getInstance().save(uid, encoded);
     }
 }
--- app/src/main/java/com/example/techdash/repositories/RecordRunRepository.java
+++ app/src/main/java/com/example/techdash/repositories/RecordRunRepository.java
@@ -1,16 +1,29 @@
 package com.example.techdash.repositories;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 
 import com.example.techdash.models.Route;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecordRunRepository {
+    private static final String TAG = RecordRunRepository.class.getSimpleName();
     private static RecordRunRepository INSTANCE;
     private MediatorLiveData<Route> route;
+    private FirebaseFirestore db;
 
     RecordRunRepository() {
         route = new MediatorLiveData<>();
+        db = FirebaseFirestore.getInstance();
     }
 
     public void addDataSource(LiveData<Route> route) {
@@ -30,5 +43,28 @@ public class RecordRunRepository {
 
     public LiveData<Route> getRoute() {
         return route;
+    }
+
+    public void save(String uid, String encoded) {
+        // TODO: save string
+        String dateTime = Calendar.getInstance().getTime().toString();
+        Map<String, Object> map = new HashMap<>();
+        map.put("route", encoded);
+
+        db.collection("users").document(uid)
+                .collection("record").document(dateTime)
+                .set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Saved record successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Failed to save record");
+                    }
+                });
     }
 }
