8ea98d91891e1d2b37baacb39591e2756659f65d
nhanlun <ntnhan18@apcs.vn>
2020-12-26 21:23:01 +0700

    Fixed bug fetching

--- app/src/main/java/com/example/techdash/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/techdash/fragments/HistoryFragment.java
@@ -1,11 +1,13 @@
 package com.example.techdash.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.techdash.R;
@@ -17,6 +19,7 @@ import java.util.ArrayList;
 
 public class HistoryFragment extends Fragment {
 
+    private static final String TAG = HistoryFragment.class.getSimpleName();
     private UserViewModel userViewModel;
 
     public HistoryFragment() {
@@ -34,7 +37,12 @@ public class HistoryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_history, container, false);
         userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
         String uid = userViewModel.getUser().getValue().getUid();
-        ArrayList<History> histories = RecordRunRepository.getInstance().fetch(uid);
+        RecordRunRepository.getInstance().fetch(uid).observe(getViewLifecycleOwner(), new Observer<ArrayList<History>>() {
+            @Override
+            public void onChanged(ArrayList<History> histories) {
+                Log.d(TAG, "history size: " + histories.size());
+            }
+        });
         return v;
     }
 }
