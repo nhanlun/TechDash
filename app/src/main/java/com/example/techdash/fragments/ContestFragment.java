71c275753633f2e270db865104a051244f286996
nickythazdess <nhat.wait4it.tan@gmail.com>
2021-05-18 23:49:42 +0700

    Contest

--- app/src/main/java/com/example/techdash/fragments/ContestFragment.java
+++ app/src/main/java/com/example/techdash/fragments/ContestFragment.java
@@ -1,15 +1,44 @@
 package com.example.techdash.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.techdash.R;
+import com.example.techdash.adapters.ContestAdapter;
+import com.example.techdash.models.Contest;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ContestFragment extends Fragment {
+    RecyclerView recyclerView;
+    private static final String TAG = ContestFragment.class.getSimpleName();
+    View v;
+    ArrayList<Contest> contestList = new ArrayList<>();
+    ContestAdapter contestAdapter;
 
     public ContestFragment() {
         // Required empty public constructor
@@ -18,12 +47,90 @@ public class ContestFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contest, container, false);
+        if (v == null) {
+            v = inflater.inflate(R.layout.fragment_contest, container, false);
+            Log.d(TAG, "New view inflated");
+        }
+
+        String date = Calendar.getInstance().getTime().toString();
+
+        contestList.add(new Contest("123", "Uni of Science", "HCMUS Marathon", "Admin",
+                date, date, new ArrayList<String>(0)));
+        contestList.add(new Contest("456", "Ho Con Rua", "Rua Marathon", "Admin",
+                date, date, new ArrayList<String>(0)));
+        contestList.add(new Contest("789", "Bitexco Tower", "Bitexco Marathon", "Admin",
+                date, date, new ArrayList<String>(0)));
+        contestList.add(new Contest("111", "Cho Ben Thanh", "Ben Thanh Marathon", "Admin",
+                date, date, new ArrayList<String>(0)));
+        contestList.add(new Contest("222", "Pho di bo Nguyen Hue", "Chay dua o Pho di bo", "Admin",
+                date, date, new ArrayList<String>(0)));
+        contestList.add(new Contest("333", "Song Sai Gon", "Chay dua tren mat song SG", "Admin",
+                date, date, new ArrayList<String>(0)));
+
+        recyclerView = v.findViewById(R.id.rvContest);
+        contestAdapter = new ContestAdapter(contestList, getContext(), R.layout.fragment_contest_row);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(contestAdapter);
+
+        /*RecordRunRepository.getInstance().fetch(uid).observe(getViewLifecycleOwner(), new Observer<ArrayList<Contest>>() {
+            @Override
+            public void onChanged(ArrayList<Contest> contests) {
+                contestAdapter.setContestArrayList(contests);
+                contestAdapter.notifyDataSetChanged();
+            }
+        });*/
+        return v;
+    }
+
+    ArrayList<Contest> filter(String s) {
+        if (s.equals("")) return contestList;
+        ArrayList<Contest> tempList = new ArrayList<Contest>();
+        //constraint is the result from text you want to filter against.
+        //objects is your data set you will filter from
+        if(contestList != null) {
+            int length = contestList.size();
+            int i=0;
+            while(i < length){
+                Contest item = contestList.get(i);
+                //do whatever you wanna do here
+                //adding result set output array
+                if (item.getName().startsWith(s))
+                    tempList.add(item);
+                i++;
+            }
+        }
+        return tempList;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        inflater.inflate(R.menu.contest_search_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search for contest name");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                contestAdapter.setContestArrayList(filter(s));
+                contestAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
--- app/src/main/java/com/example/techdash/models/Contest.java
+++ app/src/main/java/com/example/techdash/models/Contest.java
@@ -1,57 +1,134 @@
 package com.example.techdash.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.example.techdash.viewmodels.UserViewModel;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.PolyUtil;
+
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Contest {
+public class Contest implements Parcelable {
     private String id;
     private String destination;
+    private String date;
     private String name;
     private String creator_username;
-    private long startTime;
-    private long endTime;
-    private ArrayList<String> participants = new ArrayList<>();
+    private String startTime;
+    private String endTime;
+    private ArrayList<String> participants;
 
-    public Contest(Map<String, Object> map) {
-        // TODO: get the attribute here
+    public Contest() {}
+
+    public Contest(Map<String, Object> data) {
         try {
-            id = (String) map.get("id");
-            destination = (String) map.get("destination");
-            name = (String) map.get("name");
-            creator_username = (String) map.get("creator_username");
-            startTime = (long) map.get("start_time");
-            endTime = (long) map.get("end_time");
-        } catch (Exception ignored) {
+            id = (String) data.get("id");
+            destination = (String) data.get("destination");
+            name = (String) data.get("name");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis((long)data.get("starttime"));
+            startTime = calendar.getTime().toString();
+            calendar.setTimeInMillis((long)data.get("endtime"));
+            endTime = calendar.getTime().toString();
+            creator_username = (String) data.get("creator");
+            participants = (ArrayList<String>) data.get("participants");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
     }
 
-    public Contest() {
-        // TODO: remove this
-        participants.add("Who");
-        participants.add("am");
-        participants.add("I");
+    public Contest(String id, String destination, String name, String creator_username, String startTime, String endTime, ArrayList<String> participants) {
+        this.id = id;
+        this.destination = destination;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.creator_username = creator_username;
+        this.participants = participants;
     }
 
-    public ArrayList<String> getParticipants() {
-        return participants;
+    protected Contest(Parcel in) {
+        id = in.readString();
+        destination = in.readString();
+        name = in.readString();
+        startTime = in.readString();
+        endTime = in.readString();
+        creator_username = in.readString();
+        in.readStringArray(participants.toArray(new String[0]));
     }
 
-    public String getId() {
-        // TODO: replace this with the real implementation
-        return "abcxyz";
-//        return id;
+    public static final Creator<Contest> CREATOR = new Creator<Contest>() {
+        @Override
+        public Contest createFromParcel(Parcel in) {
+            return new Contest(in);
+        }
+
+        @Override
+        public Contest[] newArray(int size) {
+            return new Contest[size];
+        }
+    };
+
+    public String getID() { return id; }
+
+    public void setID(String value) { this.id = value; }
+
+    public String getDestination() { return destination; }
+
+    public void setDestination(String value) { this.destination = value; }
+
+    public String getCreator() { return creator_username; }
+
+    public void setCreator(String value) { this.creator_username = value; }
+
+    public String getName() { return name; }
+
+    public void setName(String value) { this.name = value; }
+
+    public String getStartTime() { return startTime; }
+
+    public void setStartTime(String value) { this.startTime = value; }
+
+    public String getEndTime() { return endTime; }
+
+    public void setEndTime(String value) { this.endTime = value; }
+
+    public ArrayList<String> getParticipants() { return participants; }
+
+    public void addParticipants(String value) { this.participants.add(value); }
+
+    public void removeParticipants(String value) { this.participants.remove(participants.indexOf(value)); }
+
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(destination);
+        dest.writeString(name);
+        dest.writeString(startTime);
+        dest.writeString(endTime);
+        dest.writeString(creator_username);
+        dest.writeStringArray(participants.toArray(new String[0]));
     }
 
     public HashMap<String, Object> toHashMap() {
-        // TODO: replace this with the real implementation
         HashMap<String, Object> map = new HashMap<>();
         map.put("id", id);
         map.put("destination", destination);
         map.put("name", name);
-        map.put("creator_username", creator_username);
-        map.put("startTime", startTime);
-        map.put("endTime", endTime);
+        map.put("starttime", startTime);
+        map.put("endtime", endTime);
+        map.put("creator", creator_username);
+        map.put("participants", participants);
         return map;
     }
 }
